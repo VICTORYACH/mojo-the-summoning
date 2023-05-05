0bf734994a57b2cb3231dc703b9ca03ddc5f8a7a
victoryach <achubsv@gmail.com>
2023-05-05 13:32:02 +0100

    user.js test update

--- server/src/models/User.test.js
+++ server/src/models/User.test.js
@@ -5,18 +5,33 @@ const db = require('../db/config')
 // define in global scope
 let user
 
-// clear db and create new user before tests
+//Clear db and create new user before tests
 beforeAll(async () => {
   await db.sync({ force: true })
   user = await User.create({ username: 'gandalf' })
 })
 
-// clear db after tests
+//Clear db after tests
 afterAll(async () => await db.sync({ force: true }))
 
+// Describe User
 describe('User', () => {
+  // Test 1: User has an id
   it('has an id', async () => {
     expect(user).toHaveProperty('id')
+  })
+
+  // Test 2: User has the correct username
+  it('has the correct username', async () => {
+    expect(user.username).toEqual('gandalf')
+  })
+
+  // Test 3: User can update their username
+  it('can update their username', async () => {
+    const newUsername = 'mithrandir'
+    await user.update({ username: newUsername })
+    const updatedUser = await User.findByPk(user.id)
+    expect(updatedUser.username).toEqual(newUsername)
   })
 
   /**
