4e132ad44145deab4219ff3042fc2fa68ac9ba03
victoryach <achubsv@gmail.com>
2023-05-05 14:31:14 +0100

    connected database

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,9 @@
 // add your database connection here
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('database_name', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+module.exports = sequelize;
